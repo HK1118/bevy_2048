4cd414784bf0c60423770f4003505429746f8815
HK <hinakoba0908@gmail.com>
2026-03-04 23:36:17 +0900

    add vim-like hjkl keys for movement

--- src/game/input.rs
+++ src/game/input.rs
@@ -29,13 +29,25 @@ pub(super) fn handle_input(
     keys: Res<ButtonInput<KeyCode>>,
     mut move_message: MessageWriter<Slide>,
 ) {
-    if keys.just_pressed(KeyCode::KeyW) || keys.just_pressed(KeyCode::ArrowUp) {
+    if keys.just_pressed(KeyCode::KeyW)
+        || keys.just_pressed(KeyCode::ArrowUp)
+        || keys.just_pressed(KeyCode::KeyK)
+    {
         move_message.write(Slide(Direction::Up));
-    } else if keys.just_pressed(KeyCode::KeyS) || keys.just_pressed(KeyCode::ArrowDown) {
+    } else if keys.just_pressed(KeyCode::KeyS)
+        || keys.just_pressed(KeyCode::ArrowDown)
+        || keys.just_pressed(KeyCode::KeyJ)
+    {
         move_message.write(Slide(Direction::Down));
-    } else if keys.just_pressed(KeyCode::KeyA) || keys.just_pressed(KeyCode::ArrowLeft) {
+    } else if keys.just_pressed(KeyCode::KeyA)
+        || keys.just_pressed(KeyCode::ArrowLeft)
+        || keys.just_pressed(KeyCode::KeyH)
+    {
         move_message.write(Slide(Direction::Left));
-    } else if keys.just_pressed(KeyCode::KeyD) || keys.just_pressed(KeyCode::ArrowRight) {
+    } else if keys.just_pressed(KeyCode::KeyD)
+        || keys.just_pressed(KeyCode::ArrowRight)
+        || keys.just_pressed(KeyCode::KeyL)
+    {
         move_message.write(Slide(Direction::Right));
     }
 }
